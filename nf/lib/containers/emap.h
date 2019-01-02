cf0c5528399111a4477a3a8d4dab71d3d91ff3a7
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2019-01-02 11:51:21 +0100

    Add the EMap abstraction (expiring map)
    A thin interface abstraction, EMap consists of a map, vector, and dchain.
    Perhaps it is worth packaging into a 3-tuple.

---
+++ nf/lib/containers/emap.h
@@ -0,0 +1,37 @@
+#ifndef _EMAP_H_INCLUDED_
+#define _EMAP_H_INCLUDED_
+
+#include "lib/nf_time.h"
+#include "lib/containers/map.h"
+#include "lib/containers/vector.h"
+#include "lib/containers/double-chain.h"
+
+/*@
+  fixpoint list<pair<t, int> > emap_expire_all_map<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, time_t t) {
+    return map_erase_all_fp(m, vector_get_values_fp(v, dchain_get_expired_indexes_fp(ch, t)));
+  }
+  fixpoint list<pair<t, real> > emap_expire_all_vec<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, time_t t) {
+    return vector_erase_all_fp(v, dchain_get_expired_indexes_fp(ch, t));
+  }
+  fixpoint dchain emap_expire_all_chain<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, time_t t) {
+    return dchain_expire_old_indexes_fp(ch, t);
+  }
+
+  fixpoint bool emap_has<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, t k);
+  fixpoint int emap_get<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, t k);
+  fixpoint dchain emap_rejuvente_chain<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, t k, time_t t);
+
+  fixpoint list<pair<t, int> > emap_erase_map<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, t k);
+  fixpoint list<pair<t, real> > emap_erase_vec<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, t k);
+  fixpoint dchain emap_erase_chain<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, t k);
+
+  fixpoint bool emap_has_index<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, int i);
+
+  fixpoint int emap_get_next_int<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch);
+  fixpoint dchain emap_allocate_int<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, time_t t);
+  fixpoint list<pair<t, int> > emap_add_map<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, int i, t k);
+  fixpoint list<pair<t, real> > emap_add_vec<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, int i, t k);
+  @*/
+
+
+#endif//_EMAP_H_INCLUDED_
