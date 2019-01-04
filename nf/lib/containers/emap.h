c3a4f6ecf3e2724ecb83ddb79e74530fb33ace47
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2019-01-04 14:17:24 +0100

    Encapsulate the three components of emap into a single whole
    add the emap inducative type that puts together all the three parts: map, vector
    and dchain.
    Need to adapt the other specs (bridge and balancer) to the refined abstraction.

--- nf/lib/containers/emap.h
+++ nf/lib/containers/emap.h
@@ -7,60 +7,68 @@
 #include "lib/containers/double-chain.h"
 
 /*@
-  fixpoint list<pair<t, int> > emap_expire_all_map<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, time_t t) {
-    return map_erase_all_fp(m, vector_get_values_fp(v, dchain_get_expired_indexes_fp(ch, t)));
-  }
-  fixpoint list<pair<t, real> > emap_expire_all_vec<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, time_t t) {
-    return vector_erase_all_fp(v, dchain_get_expired_indexes_fp(ch, t));
-  }
-  fixpoint dchain emap_expire_all_chain<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, time_t t) {
-    return dchain_expire_old_indexes_fp(ch, t);
+  inductive emap<t> = emap(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch);
+
+  fixpoint emap<t> emap_expire_all<t>(emap<t> em, time_t t) {
+    switch(em) { case emap(m, v, ch):
+      return emap(map_erase_all_fp(m, vector_get_values_fp(v, dchain_get_expired_indexes_fp(ch, t))),
+                  vector_erase_all_fp(v, dchain_get_expired_indexes_fp(ch, t)),
+                  dchain_expire_old_indexes_fp(ch, t));
+    }
   }
 
-  fixpoint bool emap_has<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, t k) {
-    return map_has_fp(m, k);
+  fixpoint bool emap_has<t>(emap<t> em, t k) {
+    switch(em) { case emap(m, v, ch):
+      return map_has_fp(m, k);
+    }
   }
-  fixpoint int emap_get<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, t k) {
-    return map_get_fp(m, k);
+  fixpoint int emap_get<t>(emap<t> em, t k) {
+    switch(em) { case emap(m, v, ch):
+      return map_get_fp(m, k);
+    }
   }
-  fixpoint dchain emap_rejuvenate_idx_chain<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, int i, time_t t) {
-    return dchain_rejuvenate_fp(ch, i, t);
-  }
-  //FIXME: delete this, can be expressed through rejuv_idx.
-  fixpoint dchain emap_rejuvenate_chain<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, t k, time_t t) {
-    return dchain_rejuvenate_fp(ch, map_get_fp(m, k), t);
-  }
-
-  fixpoint list<pair<t, int> > emap_erase_map<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, t k) {
-    return map_erase_fp(m, k);
-  }
-  fixpoint list<pair<t, real> > emap_erase_vec<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, t k) {
-    return update(map_get_fp(m, k), pair(k, 1.0), v);
-  }
-  fixpoint dchain emap_erase_chain<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, t k) {
-    return dchain_remove_index_fp(ch, map_get_fp(m, k));
+  fixpoint emap<t> emap_rejuvenate_idx<t>(emap<t> em, int i, time_t t) {
+    switch(em) { case emap(m, v, ch):
+      return emap(m, v, dchain_rejuvenate_fp(ch, i, t));
+    }
   }
 
-  fixpoint bool emap_has_index<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, int i) {
-    return dchain_allocated_fp(ch, i);
+  fixpoint emap<t> emap_erase<t>(emap<t> em, t k) {
+    switch(em) { case emap(m, v, ch):
+      return emap(map_erase_fp(m, k),
+                  update(map_get_fp(m, k), pair(k, 1.0), v),
+                  dchain_remove_index_fp(ch, map_get_fp(m, k)));
+    }
   }
 
-  fixpoint t emap_get_key<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, int i) {
-    return fst(nth(i, v));
+  fixpoint bool emap_has_index<t>(emap<t> em, int i) {
+    switch(em) { case emap(m, v, ch):
+      return dchain_allocated_fp(ch, i);
+    }
   }
 
-  fixpoint dchain emap_allocate_int<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, time_t t, int i) {
-    return dchain_allocate_fp(ch, i, t);
-  }
-  fixpoint list<pair<t, int> > emap_add_map<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, t k, int i) {
-    return map_put_fp(m, k, i);
-  }
-  fixpoint list<pair<t, real> > emap_add_vec<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch, t k, int i) {
-    return update(i, pair(k, 0.75), v);
+  fixpoint t emap_get_key<t>(emap<t> em, int i) {
+    switch(em) { case emap(m, v, ch):
+      return fst(nth(i, v));
+    }
   }
 
-  fixpoint bool emap_full<t>(list<pair<t, int> > m, list<pair<t, real> > v, dchain ch) {
-    return dchain_out_of_space_fp(ch); //dchain_index_range_fp(ch) <= length(m);
+  fixpoint emap<t> emap_allocate_int<t>(emap<t> em, time_t t, int i) {
+    switch(em) { case emap(m, v, ch):
+      return emap(m, v, dchain_allocate_fp(ch, i, t));
+    }
+  }
+
+  fixpoint emap<t> emap_add_map<t>(emap<t> em, t k, int i) {
+    switch(em) { case emap(m, v, ch):
+      return emap(map_put_fp(m, k, i), update(i, pair(k, 0.75), v), ch);
+    }
+  }
+
+  fixpoint bool emap_full<t>(emap<t> em) {
+    switch(em) { case emap(m, v, ch):
+      return dchain_out_of_space_fp(ch);
+    }
   }
   @*/
 
