19bd703a84b11619b18dab878c6ef347966cd2e7
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2019-01-22 12:16:26 +0100

    Certify the packet-buffer management API calls

--- nf/lib/packet-io.h
+++ nf/lib/packet-io.h
@@ -18,6 +18,7 @@ struct rte_mempool;
 void packet_borrow_next_chunk(void* p, size_t length, void** chunk);
 /*@ requires packetp(p, ?unread, ?mc) &*&
              length <= length(unread) &*&
+             0 < length &*& length < INT_MAX &*&
              *chunk |-> _; @*/
 /*@ ensures *chunk |-> ?ptr &*&
             packetp(p, drop(length, unread), cons(pair(ptr, length), mc)) &*&
@@ -27,6 +28,12 @@ void packet_return_chunk(void* p, void* chunk);
 /*@ requires packetp(p, ?unread, cons(pair(chunk, ?len), ?mc)) &*&
              chars(chunk, len, ?chnk); @*/
 /*@ ensures packetp(p, append(chnk, unread), mc); @*/
+
+uint32_t packet_get_unread_length(void* p);
+/*@ requires packetp(p, ?unread, ?mc); @*/
+/*@ ensures packetp(p, unread, mc) &*&
+            result == length(unread); @*/
+
 
 bool packet_receive(uint16_t src_device, void** p, uint16_t* len);
 /*@ requires *p |-> _; @*/
@@ -47,11 +54,6 @@ void packet_flood(void* p, uint16_t skip_device, uint16_t nb_devices);
 void packet_free(void* p);
 /*@ requires packetp(p, _, nil); @*/
 /*@ ensures true; @*/
-
-uint32_t packet_get_unread_length(void* p);
-/*@ requires packetp(p, ?unread, ?mc); @*/
-/*@ ensures packetp(p, unread, mc) &*&
-            result == length(unread); @*/
 
 void packet_clone(void* src, void** clone);
 /*@ requires packetp(src, ?unread, ?mc) &*& *clone |-> _; @*/
