30f83bf651cf701db885836c2f6f0dd3f950977a
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2019-02-01 16:07:52 +0100

    Missing files for the bridge data-record codegen

---
+++ nf/vigbridge/stat_key.h
@@ -0,0 +1,12 @@
+#ifndef _STAT_KEY_H_INCLUDED_
+#define _STAT_KEY_H_INCLUDED_
+
+#include <stdint.h>
+#include "ether_addr.h"
+
+struct StaticKey {
+  struct ether_addr addr;
+  uint16_t device;
+};
+
+#endif//_STAT_KEY_H_INCLUDED_
