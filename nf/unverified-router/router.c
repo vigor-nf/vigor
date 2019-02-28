e9aa088c0f420041fb8c27ab2dcd50cf47371fe7
Hédi <hedi.sassi@epfl.ch>
2019-02-28 23:01:25 +0100

    parsing of ip done but needs testing

--- nf/unverified-router/router.c
+++ nf/unverified-router/router.c
@@ -1,11 +1,14 @@
 #include "lpm_trie/lpm_trie_mem.h"
 #include <stdio.h>
+#include <math.h>
+#include <ctype.h>
 
 #define IPV4_IP_SIZE 4
 #define CSV_LINE_SIZE 17
 
 int insert_all(FILE * f, struct lpm_trie * t);
 struct lpm_trie_key *lpm_trie_key_alloc(size_t prefixlen, uint8_t *data);
+char * take(size_t starting, size_t n, const char * s, size_t length);
 
 int main( int argc, const char* argv[] ){
 	
@@ -29,7 +32,11 @@ int main( int argc, const char* argv[] ){
 	
 	//insert all routes into data structure
 	
-	int res = insert_all(in_file, trie)
+	int res = insert_all(in_file, trie);
+	
+	if(res != 0){
+		exit(-1);
+	}
     
     //close file
     fclose(in_file);
@@ -41,9 +48,89 @@ int main( int argc, const char* argv[] ){
 	trie = NULL;
 }
 
+/**
+ * Transform a small string in an integer between 0-255
+ */
+uint8_t get_number(const char * s, size_t size){
+	
+	int buffer = 0;
+	
+	for(size_t i = 0; i < size; ++i){
+		
+		if(! isdigit(s[i])){
+			printf("Error while parsing routes, invalid ip !\n"); 
+			exit(-1);
+		}
+		
+		buffer += pow(10,size -i -1);
+	}
+	if(buffer > 255){
+		
+		printf("Error while parsing routes, invalid ip !\n"); 
+		exit(-1);
+	}
+	
+	return (uint8_t)buffer;
+}
 
+/**
+ * Transform a string that represents an ip address in a list of integers between 0-255
+ */
+uint8_t * parse_ip(char * ip, size_t size){
 
+	if(ip == NULL){
+		return NULL;
+	}
+	
+	uint8_t * res = calloc(IPV4_IP_SIZE, sizeof(uint8_t));
+	
+	if(! res){
+		return NULL;
+	}
+	
+	size_t count = 0;
+	size_t j = 0;
+	
+	for(size_t i = 0; i < size; ++i){
+		
+		if(ip[i] == '.'){
+				
+			res[j] = get_number(take(i - count -1, count, ip, size), count);
+			count = 0;
+			j++;
+		}
+		count++;
+		
+	}
+	
+	free(ip);
+	ip = NULL;
+	
+	return res;
+	
+}
 
+/**
+ * Takes n elements of a string of size = length starting at index = starting
+ */
+char * take(size_t starting, size_t n, const char * s, size_t length){
+	
+	char * res = calloc(n, sizeof(char));
+	if(!res || n + starting > length){
+		return NULL;
+	}
+	
+	for(size_t i = starting; i < n; ++i){
+		
+		res[i] = s[i];
+	}
+	
+	return res;
+}
+
+/**
+ * insert all routes from the csv file to the lpm trie
+ */
 int insert_all(FILE * f, struct lpm_trie * t){
 	
     size_t length = 0;
@@ -51,27 +138,38 @@ int insert_all(FILE * f, struct lpm_trie * t){
     size_t csvLength = 0;
     
 	
-	while ((csvLength = getline(line, &lenght, f)) == CSV_LINE_SIZE) {
+	while ((csvLength = getline(&line, &length, f)) != -1) {
 		
 		struct lpm_trie_key *key = malloc(sizeof(struct lpm_trie_key));
+		
+		if(!key){
+			printf("Could not allocate memory !\n");
+			return -1;
+		}
     
-		uint8_t * ip = calloc(IPV4_IP_SIZE, sizeof(uint8_t);
+		uint8_t * ip = NULL;
 		uint32_t mask = 0;
 		int port = 0;
 		int j = 0;
 		
 		for(size_t i = 0; i < length; ++i){
 		
-			if(csv[i] == ','){
+			if(line[i] == ','){
 				if( j == 0){
-					ip = parse_ip()
+					ip = parse_ip(take(0,i,line, csvLength), i);
+					
+					if(!ip){
+						printf("Error while parsing routes !\n"); 
+						return -1;
+					}
+					
 					j++;
 				}
 				else if(j == 1){
-					mask = 
+					//mask = 
 				}
 				else{
-					port = 
+					//port = 
 				}
 			}
 			
@@ -81,16 +179,10 @@ int insert_all(FILE * f, struct lpm_trie * t){
 		key->prefixlen = mask;
 		memcpy(key->data, ip, IPV4_IP_SIZE);
 		
-		int res = trie_update_elem(t, key_1, value_1);
+		int res = trie_update_elem(t, key, port);
+		
       
-      
-      
-    }
-
-	
-	
-	
-	/*
+      /*
 	int value_1 = 1;
     int res = trie_update_elem(t, key_1, value_1);
     if(res)
@@ -103,7 +195,17 @@ int insert_all(FILE * f, struct lpm_trie * t){
   
   
         */
-        
+      
+      
+		if(ip){
+			free(ip);
+			ip = NULL;
+		}
+    }
+
+	
+	
+	
         
        
     if(line){
