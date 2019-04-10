34784ddff9e67c44386a2aab2e72480e8e49357e
rishabh246 <rishabh246@gmail.com>
2019-04-10 12:52:00 +0200

    Typos

--- nf/testbed/hard/run.sh
+++ nf/testbed/hard/run.sh
@@ -11,7 +11,7 @@
 #     Otherwise, a folder name containing a DPDK NAT-like app, e.g. "~/vnds/nat"
 # $2: The scenario, one of the following:
 #     "mg-1p": Measure throughput: find the rate at which the middlebox
-#              starts loosing 1% of packets.
+#              starts losing 1% of packets.
 #     "mg-existing-flows-latency": Measure the forwarding latency for existing
 #                                  flows.
 #     "mg-new-flows-latency": Measure the forwarding latency for new flows.
