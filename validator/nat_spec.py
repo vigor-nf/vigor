34bf04b788a7a6a9a3aaec318755eae011c9355c
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2019-04-14 13:15:17 +0200

    New syntax for header demarshalling in python spec
    Also rendering of that demarshalling: 3xswitch per header in VF code

--- validator/nat_spec.py
+++ validator/nat_spec.py
@@ -5,26 +5,16 @@ EXT_PORT = 1
 flow_emap = emap(initial_fm, initial_fv, initial_heap)
 final_flow_emap = emap(final_flow_map, final_flow_vec, final_flow_chain)
 
-if (EXP_TIME < now): # consider only normal moments, remote from the start of the epoch
-    if (a_packet_received):
+if EXP_TIME < now: # consider only normal moments, remote from the start of the epoch
+    if a_packet_received: # This is probably implied by the first pop_header call, TODO: check
         flow_emap = emap_expire_all(flow_emap, now - EXP_TIME)
 
-        if (recv_headers == []):
-            assert sent_on_ports == []
-        [h1, *t1] = recv_headers
-        if (h1.protocol != ether):
-            assert sent_on_ports == []
-        if (t1 == []):
-            assert sent_on_ports == []
-        [h2, *t2] = t1
-        if (h2.protocol != ip):
-            assert sent_on_ports == []
-        if (t2 == []):
-            assert sent_on_ports == []
-        [h3, *t3] = t2
-        if (h3.protocol != tcp_udp):
-            assert sent_on_ports == []
+        h1 = pop_header(ether, on_mismatch=([],[]))
+        h2 = pop_header(ipv4, on_mismatch=([],[]))
+        h3 = pop_header(tcp_udp, on_mismatch=([],[]))
 
+        # TODO: VVV replace this with assertions, as we already expect the ether/ip/tcpudp stack
+        # because of the above 3 calls to pop_header
         if ((h1.type & 0x10) == 0x10): # 0x10 -> IPv4
             if (h2.inpid == 6 or h2.inpid == 17): # 6/17 -> TCP/UDP
                 if (received_on_port == EXT_PORT):
@@ -34,35 +24,35 @@ if (EXP_TIME < now): # consider only normal moments, remote from the start of th
                         flow_emap = emap_refresh_idx(flow_emap, flow_indx, now)
                         if (internal_flow.sp != h2.isaddr or
                             internal_flow.dp != h2.isrc_port or
-                            prot != h2.inpid):
-                            assert sent_on_ports == []
+                            internal_flow.prot != h2.inpid):
+                            return ([],[])
                         else:
-                            assert sent_on_ports == [internal_flow.idev]
-                            assert sent_headers == [ether(h1, saddr=..., daddr=...),
-                                                    ipv4(h2, icksum=..., saddr=internal_flow.dip, daddr=internal_flow.sip),
-                                                    tcpudp(src_port=internal_flow.dp, dst_port=internal_flow.sp)]
+                            return ([internal_flow.idev],
+                                    [ether(h1, saddr=..., daddr=...),
+                                     ipv4(h2, icksum=..., saddr=internal_flow.dip, daddr=internal_flow.sip),
+                                     tcpudp(src_port=internal_flow.dp, dst_port=internal_flow.sp)])
                     else:
-                        assert sent_on_ports == []
+                        return ([],[])
                 else: # packet from the internal network
                     internal_flow_id = FlowId(isrc_port, idst_port, isaddr, idaddr, received_on_port, inpid)
                     if (emap_has(flow_emap, internal_flow_id)): # flow present in the table
                         idx = emap_get(flow_emap, internal_flow_id)
                         flow_emap = emap_refresh_idx(flow_emap, idx, now)
-                        assert sent_on_ports == [EXT_PORT]
-                        assert sent_headers == [ether(h1, saddr=..., daddr=...),
-                                                ipv4(h2, icksum=..., saddr=EXT_IP_ADDR),
-                                                tcpudp(h3, src_port=idx + start_port)]
+                        return ([EXT_PORT],
+                                [ether(h1, saddr=..., daddr=...),
+                                 ipv4(h2, icksum=..., saddr=EXT_IP_ADDR),
+                                 tcpudp(h3, src_port=idx + start_port)])
                     else: # No flow in the table
                         if (emap_full(flow_emap)): # flowtable overflow
-                            assert sent_on_ports == []
+                            return ([],[])
                         else:
                             idx = the_index_allocated
                             flow_emap = emap_add(flow_emap, internal_flow_id, idx, now)
-                            assert sent_on_ports == [EXT_PORT]
-                            assert sent_headers == [ether(h1, saddr=..., daddr=...),
-                                                    ipv4(h2, icksum=..., saddr=EXT_IP_ADDR, daddr=internal_flow_id.sip),
-                                                    tcpudp(h3, src_port=idx + start_port)]
+                            return ([EXT_PORT],
+                                    [ether(h1, saddr=..., daddr=...),
+                                     ipv4(h2, icksum=..., saddr=EXT_IP_ADDR, daddr=internal_flow_id.sip),
+                                     tcpudp(h3, src_port=idx + start_port)])
             else: # Non TCP or UDP packet
-                assert sent_on_ports == []
+                return ([],[])
         else: # Non IPv4 packet
-            assert sent_on_ports == []
+            return ([],[])
--- validator/translate-spec.py
+++ validator/translate-spec.py
@@ -12,25 +12,116 @@ def render_expr(expr):
     elif isinstance(expr, ast.Call):
         args = ", ".join(list(map(render_expr, expr.args)))
         return "{}({})".format(expr.func.id, args)
+    elif isinstance(expr, ast.Compare):
+        left = render_expr(expr.left)
+        assert len(expr.ops) == 1
+        relation = expr.ops[0]
+        assert len(expr.comparators) == 1
+        right = render_expr(expr.comparators[0])
+        if   isinstance(relation, ast.Lt): sign = '<'
+        elif isinstance(relation, ast.Eq): sign = '=='
+        elif isinstance(relation, ast.NotEq): sign = '!='
+        elif isinstance(relation, ast.Gt): sign = '>'
+        else: sign = '???'
+        return "({} {} {})".format(left, sign, right)
+    elif isinstance(expr, ast.BinOp):
+        left = render_expr(expr.left)
+        right = render_expr(expr.right)
+        if   isinstance(expr.op, ast.Sub): sign = '-'
+        elif isinstance(expr.op, ast.Add): sign = '+'
+        elif isinstance(expr.op, ast.BitAnd): sign = '&'
+        else: sign = '???'
+        return "({} {} {})".format(left, sign, right)
+    elif isinstance(expr, ast.BoolOp):
+        left = render_expr(expr.values[0])
+        right = render_expr(expr.values[1])
+        if   isinstance(expr.op, ast.And): sign = '&&'
+        elif isinstance(expr.op, ast.Or): sign = '||'
+        else: sign = '???'
+        return "(" + (sign.join(map(render_expr, expr.values))) + ")"
+    elif isinstance(expr, ast.List):
+        result = ""
+        for e in expr.elts:
+            result = "cons(" + render_expr(e) + ", " + result
+        return result + "nil" + (")" * len(expr.elts))
+
     else:
         return "complicated"
 
+def genOutcome(ports_headers):
+    assert isinstance(ports_headers, ast.Tuple)
+    assert len(ports_headers.elts) == 2
+    ports = ports_headers.elts[0]
+    headers = ports_headers.elts[1]
+    assert isinstance(ports, ast.List)
+    assert isinstance(headers, ast.List)
+    if ports.elts:
+        return "assert sent_on_ports == {} && sent_headers == {};".format(render_expr(ports), render_expr(headers))
+    else:
+        return "assert sent_on_ports == [];"
+
+def isPopHeader(expr):
+    if (not isinstance(expr, ast.Assign) or
+        len(expr.targets) != 1):
+        return False
+    target = expr.targets[0]
+    value = expr.value
+    if (not isinstance(target, ast.Name) or
+        not isinstance(value, ast.Call) or
+        not isinstance(value.func, ast.Name) or
+        value.func.id != 'pop_header'):
+        return False
+    assert len(value.keywords) == 1
+    assert value.keywords[0].arg == 'on_mismatch'
+    assert len(value.args) == 1
+    assert isinstance(value.args[0], ast.Name)
+    return True
+
+protocol_headers = {'ether':['saddr', 'daddr', 'type'],
+                    'ipv4':['vihl,' 'tos, ''len, ''pid, ''foff,'
+                            'ttl, ''npid,' 'cksum', 'saddr', 'daddr'],
+                    'tcp_udp':['src_port', 'dst_port']}
+header_stack = "recv_headers"
+def translatePopHeader(binding, body):
+    global header_stack
+    print("switch({}) {{\n".format(header_stack))
+    on_mismatch = genOutcome(binding.value.keywords[0].value)
+    print("case nil: {}".format(on_mismatch))
+    header_stack_tail = header_stack + "_t"
+    header = "h1" # TODO: generate unique name
+    print("case cons({}, {}):".format(header, header_stack_tail))
+    header_stack = header_stack_tail
+    print("switch({}) {{".format(header))
+    protocol = binding.value.args[0].id
+    assert protocol in protocol_headers
+    for p in protocol_headers.keys():
+        if p != protocol:
+            print("case {}(dummy): {}".format(p + '_hdr', on_mismatch))
+    fields = protocol_headers[protocol]
+    hdr_name = protocol + '_hdr_shell'
+    print("case {}({}): switch({}) {{".format(protocol + '_hdr', hdr_name, hdr_name))
+    print("case {}({}): ".format(protocol + '_hdrc', ", ".join(fields)))
+    translate(body)
+    print("}}}")
+
 def translate(exprList):
-    for expr in exprList:
+    while exprList:
+        [expr, *exprList] = exprList
         if isinstance(expr, ast.Assign):
             value = render_expr(expr.value)
             assert isinstance(expr.targets, list)
             target = expr.targets[0]
             if isinstance(target, ast.Name):
+                if isPopHeader(expr):
+                    translatePopHeader(expr, exprList)
+                    return
                 assert len(expr.targets) == 1
                 if target.id.isupper():
                     print("#define {} ({})".format(target.id, value))
                 else:
                     print("{} = {};".format(target.id, value))
-            elif isinstance(target, ast.List):
-                assert len(target.elts) == 2
-                assert isinstance(target.elts[1], ast.Starred)
-                print("list destructuring")
+            else:
+                print("Weird assignment")
         elif isinstance(expr, ast.If):
             print("if ({}) {{".format(render_expr(expr.test)))
             translate(expr.body)
