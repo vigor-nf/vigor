667d71af1fbdc170c81286054a016391a5f2e42f
Matteo Rizzo <matteo.rizzo@epfl.ch>
2019-05-06 18:04:23 +0200

    Make kernel bootable with iPXE

--- nf/lib/kernel/asm/multiboot_header.asm
+++ nf/lib/kernel/asm/multiboot_header.asm
@@ -1,16 +1,8 @@
 section .multiboot_header
 
 header_start:
-    dd 0xe85250d6                ; magic number (multiboot 2)
-    dd 0                         ; architecture 0 (protected mode i386)
-    dd header_end - header_start ; header length
-    ; checksum
-    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+    dd 0x1BADB002        ; magic number (multiboot 1)
+    dd 3                 ; multiboot flags
+    dd -(0x1BADB002 + 3) ; header checksum
 
-    ; insert optional multiboot tags here
-
-    ; required end tag
-    dw 0    ; type
-    dw 0    ; flags
-    dd 8    ; size
 header_end:
