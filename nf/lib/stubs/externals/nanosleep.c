e8f294545a38cfd89d4eeadddcfea84cfe16b968
Matteo Rizzo <matteo.rizzo@epfl.ch>
2019-05-17 13:40:27 +0200

    Change nanosleep stub to assert(0)

--- nf/lib/stubs/externals/nanosleep.c
+++ nf/lib/stubs/externals/nanosleep.c
@@ -1,7 +1,7 @@
+#include <assert.h>
 #include <time.h>
 
 int nanosleep(const struct timespec *req, struct timespec *rem)
 {
-	/* Don't actually sleep. TODO: Check if this is fine */
-	return 0;
+	assert(0);
 }
