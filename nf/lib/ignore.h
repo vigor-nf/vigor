047a7dff7b223b14617c8b1dc7c2feb8f1152a8b
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2019-07-23 14:52:03 +0200

    Simplify IGNORE(x): find the formula that works both in verifast and compilers

--- nf/lib/ignore.h
+++ nf/lib/ignore.h
@@ -1,10 +1,7 @@
+#include <stdint.h>
 #include "include_ignored_by_verifast.h"
 /**
    An auxilary header, defining the IGNORE macro to tell the compiler that a
    certain function parameter is not used.
 */
-#ifdef _NO_VERIFAST_
-#  define IGNORE(x) (void)(x)
-#else //_NO_VERIFAST_
-#  define IGNORE(x)
-#endif //_NO_VERIFAST_
+#define IGNORE(x) (uintptr_t)(x)
