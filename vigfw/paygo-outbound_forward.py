4f009df600a6aad514872ca15727b92dda644b1a
Solal Pirelli <solal.pirelli@gmail.com>
2019-08-09 19:55:16 +0200

    Move fw paygo props

--- vigfw/paygo-outbound_forward.py
+++ vigfw/paygo-outbound_forward.py
@@ -1,8 +1,8 @@
 from state import flow_emap, int_devices
-EXP_TIME = 10
+EXP_TIME = 10 * 1000
 EXT_DEVICE = 1
 
-if a_packet_received and EXP_TIME <= now:
+if a_packet_received:
     flow_emap.expire_all(now - EXP_TIME)
 
 h3 = pop_header(tcpudp, on_mismatch=([],[]))
@@ -10,11 +10,10 @@ h2 = pop_header(ipv4, on_mismatch=([],[]))
 h1 = pop_header(ether, on_mismatch=([],[]))
 
 internal_flow = FlowIdc(h3.src_port, h3.dst_port, h2.saddr, h2.daddr, h2.npid)
-if (received_on_port != EXT_DEVICE and
-    not flow_emap.has(internal_flow) and
-    not flow_emap.full()) :
-    fl_id = the_index_allocated
-    flow_emap.add(internal_flow, fl_id, now)
-    vector_set(int_devices, fl_id, received_on_port)
+if received_on_port != EXT_DEVICE:
+    return ([EXT_DEVICE],[ether(h1, saddr=..., daddr=...),
+                          ipv4(h2, cksum=...),
+                          tcpudp(h3)])
+    pass # Ignore the state change
 else:
     pass
