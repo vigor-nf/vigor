aea46271c933cd385ca3c2b7a203fef6a7df850d
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2019-08-20 17:01:35 +0200

    [Style]clang-format unverified-nat NF

--- private/unverified-nat/nat_map_dpdk.c
+++ private/unverified-nat/nat_map_dpdk.c
@@ -14,106 +14,94 @@
 // they're opaque (modulo an user-configured offset, which we force to be 0).
 // Since this is C, well... a pointer is a pointer is a pointer.
 
-
 struct nat_map {
-	void* value;
+  void *value;
 };
 
 static nat_map_hash_fn map_hash_fn;
 
-static uint64_t
-nat_map_hash_fn_dpdk(void* key, void* key_mask, uint32_t key_size, uint64_t seed)
-{
-	return (*map_hash_fn)(*((nat_flow_id*) key));
+static uint64_t nat_map_hash_fn_dpdk(void *key, void *key_mask,
+                                     uint32_t key_size, uint64_t seed) {
+  return (*map_hash_fn)(*((nat_flow_id *)key));
 }
 
+void nat_map_set_fns(nat_map_hash_fn hash_fn, nat_map_eq_fn eq_fn) {
+  (void)eq_fn; // Unused
 
-void
-nat_map_set_fns(nat_map_hash_fn hash_fn, nat_map_eq_fn eq_fn)
-{
-	(void) eq_fn; // Unused
-
-	map_hash_fn = hash_fn;
+  map_hash_fn = hash_fn;
 }
 
-struct nat_map*
-nat_map_create(uint32_t capacity)
-{
-	rte_table_hash_params table_params;
-	table_params.name = "table";
-	table_params.key_size = sizeof(nat_flow_id);
-	table_params.key_offset = 0; // MUST be 0, see remark at top of file
-	table_params.key_mask = NULL; // unused
-	table_params.n_keys = capacity;
-	table_params.n_buckets = capacity >> 2;
-	table_params.f_hash = &nat_map_hash_fn_dpdk;
-	table_params.seed = 0; // unused
+struct nat_map *nat_map_create(uint32_t capacity) {
+  rte_table_hash_params table_params;
+  table_params.name = "table";
+  table_params.key_size = sizeof(nat_flow_id);
+  table_params.key_offset = 0;  // MUST be 0, see remark at top of file
+  table_params.key_mask = NULL; // unused
+  table_params.n_keys = capacity;
+  table_params.n_buckets = capacity >> 2;
+  table_params.f_hash = &nat_map_hash_fn_dpdk;
+  table_params.seed = 0; // unused
 
-	// 2nd param is socket ID, we don't really need it
-	void* dpdk_table = rte_table_hash_ext_ops.f_create(&table_params, 0, sizeof(nat_flow*));
-	if (dpdk_table == NULL) {
-		rte_exit(EXIT_FAILURE, "Out of memory in nat_map_create for rte_table\n");
-	}
+  // 2nd param is socket ID, we don't really need it
+  void *dpdk_table =
+      rte_table_hash_ext_ops.f_create(&table_params, 0, sizeof(nat_flow *));
+  if (dpdk_table == NULL) {
+    rte_exit(EXIT_FAILURE, "Out of memory in nat_map_create for rte_table\n");
+  }
 
-	nat_map* map = (nat_map*) malloc(sizeof(nat_map));
-	if (map == NULL) {
-		rte_exit(EXIT_FAILURE, "Out of memory in nat_map_create for nat_map\n");
-	}
+  nat_map *map = (nat_map *)malloc(sizeof(nat_map));
+  if (map == NULL) {
+    rte_exit(EXIT_FAILURE, "Out of memory in nat_map_create for nat_map\n");
+  }
 
-	map->value = dpdk_table;
-	return map;
+  map->value = dpdk_table;
+  return map;
 }
 
-void
-nat_map_insert(struct nat_map* map, nat_flow_id key, nat_flow* value)
-{
-	// The add function allows to both check if the value was already there, and get a handle to the entry.
-	// We care about neither.
-	int unused_key_found;
-	void* unused_entry_ptr;
+void nat_map_insert(struct nat_map *map, nat_flow_id key, nat_flow *value) {
+  // The add function allows to both check if the value was already there, and
+  // get a handle to the entry. We care about neither.
+  int unused_key_found;
+  void *unused_entry_ptr;
 
-	int ret = rte_table_hash_ext_ops.f_add(map->value, &key, &value, &unused_key_found, &unused_entry_ptr);
-	if (ret != 0) {
-		rte_exit(ret, "Error in nat_map_insert\n");
-	}
+  int ret = rte_table_hash_ext_ops.f_add(map->value, &key, &value,
+                                         &unused_key_found, &unused_entry_ptr);
+  if (ret != 0) {
+    rte_exit(ret, "Error in nat_map_insert\n");
+  }
 }
 
-void
-nat_map_remove(struct nat_map* map, nat_flow_id key)
-{
-	// Same remark as insert
-	int unused_key_found;
-	void* unused_entry_ptr;
+void nat_map_remove(struct nat_map *map, nat_flow_id key) {
+  // Same remark as insert
+  int unused_key_found;
+  void *unused_entry_ptr;
 
-	int ret = rte_table_hash_ext_ops.f_delete(map->value, &key, &unused_key_found, &unused_entry_ptr);
-	if (ret != 0) {
-		rte_exit(ret, "Error in nat_map_remove\n");
-	}
+  int ret = rte_table_hash_ext_ops.f_delete(map->value, &key, &unused_key_found,
+                                            &unused_entry_ptr);
+  if (ret != 0) {
+    rte_exit(ret, "Error in nat_map_remove\n");
+  }
 }
 
-bool
-nat_map_get(struct nat_map* map, nat_flow_id key, nat_flow** value)
-{
-	uint64_t lookup_hit_mask;
-	void* keys = &key;
-	// rte_table requires values to be a fully valid 64-entry array
-	void* values[64];
+bool nat_map_get(struct nat_map *map, nat_flow_id key, nat_flow **value) {
+  uint64_t lookup_hit_mask;
+  void *keys = &key;
+  // rte_table requires values to be a fully valid 64-entry array
+  void *values[64];
 
-	int ret = rte_table_hash_ext_ops.f_lookup(
-		map->value,
-		(struct rte_mbuf**) &keys, // keys: pseudo-array of pseudo-mbufs
-		RTE_LEN2MASK(1, uint64_t), // bitmask of valid keys
-		&lookup_hit_mask,
-		values
-	);
-	if (ret != 0) {
-		rte_exit(ret, "Error in nat_map_get\n");
-	}
+  int ret = rte_table_hash_ext_ops.f_lookup(
+      map->value,
+      (struct rte_mbuf **)&keys, // keys: pseudo-array of pseudo-mbufs
+      RTE_LEN2MASK(1, uint64_t), // bitmask of valid keys
+      &lookup_hit_mask, values);
+  if (ret != 0) {
+    rte_exit(ret, "Error in nat_map_get\n");
+  }
 
-	if (lookup_hit_mask == 0) {
-		return false;
-	}
+  if (lookup_hit_mask == 0) {
+    return false;
+  }
 
-	*value = *((nat_flow**) values[0]);
-	return true;
+  *value = *((nat_flow **)values[0]);
+  return true;
 }
