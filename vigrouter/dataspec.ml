e8c61330a31edac5f7144986daacedd179ef23a4
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2019-08-29 12:30:01 +0200

    Make vigrouter compile (given the new codegen inputs)

--- vigrouter/dataspec.ml
+++ vigrouter/dataspec.ml
@@ -1,4 +1,12 @@
 open Data_spec
+open Core
+open Ir
 
 let containers = ["lpm", LPM ("route_condition");]
 let custom_includes = []
+
+(* TODO: add constraints *)
+let constraints = []
+
+let gen_custom_includes : string list ref = ref []
+let gen_records : (string * ttype) list ref = ref []
