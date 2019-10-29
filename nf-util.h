5ee21c4a14054d909e753ed498ff7142359b44af
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2019-10-29 23:05:20 +0100

    fix the nf_send

--- nf-util.h
+++ nf-util.h
@@ -179,7 +179,7 @@ static inline void nf_free_packet(struct rte_mbuf *mbuf) {
 
 static inline void nf_send_packet(struct rte_mbuf *mbuf, int dst_device) {
   uint16_t actual_tx_len = rte_eth_tx_burst(dst_device, 0, &mbuf, 1);
-  if (actual_tx_len == 1) {
+  if (actual_tx_len != 1) {
     rte_exit(EXIT_FAILURE, "We assume the hardware will allways accept a packet for send.\n");
   }
   return;
